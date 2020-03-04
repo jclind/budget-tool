6b2eacd5def2bc46bf2f74b3a029cd5e1f546dea
Jesse Lind <Jesseclind@gmail.com>
2020-03-04 02:58:22 -0500

    add delete function, changing balance/color, and local storage

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
-let initialBalance = 0;
-let expensesAmount = 0;
-let currentBalance = 0;
-const expensesList = [];    
+let initialBalance = JSON.parse(localStorage.getItem("initialBalance"));
+let expensesAmount = JSON.parse(localStorage.getItem("expensesAmount")); ;
+let currentBalance = JSON.parse(localStorage.getItem("currentBalance")); ;
+const expensesList = JSON.parse(localStorage.getItem("expensesList"));    
 
-console.log("hello")
 
 function add() {
     let inputIncomeTitle = document.getElementById('input-income-title').value;
@@ -20,16 +19,20 @@ function add() {
 
     // add input value to total and current balances
     initialBalance += Number(inputIncomeAmount);
-    document.getElementById('initial-balance').innerHTML = `$${initialBalance}`;
     currentBalance += Number(inputIncomeAmount);
-    document.getElementById('current-balance').innerHTML = `$${currentBalance}`;
+    updateBalance();
 
     // add title and value to array and display it using createList();
-    let temp = {'title': inputIncomeTitle, 'value': inputIncomeAmount};
+    let temp = {'title': inputIncomeTitle, 'value': inputIncomeAmount, 'expense': false};
     expensesList.push(temp);
-    document.getElementById('table-body').innerHTML = '';
+    document.getElementById('table-body').innerHTML = '';1
     createList();
-    
+    changeColor();
+
+    // clear input boxes
+    document.getElementById('input-income-title').value = '';
+    document.getElementById('input-income-amount').value = '';
+    storage();
 }
 function subtract() {
     let inputExpensesTitle = document.getElementById('input-expenses-title').value;
@@ -45,62 +48,131 @@ function subtract() {
         return false;
     }
 
+    // increase expenses counter and decrease current balance
     expensesAmount += Number(inputExpensesAmount);
-    document.getElementById('expenses').innerHTML = `$${expensesAmount}`;
     currentBalance -= Number(inputExpensesAmount);
-    document.getElementById('current-balance').innerHTML = `$${currentBalance}`
+    updateBalance();
 
+    // add expense to the expensesList array
     let temp = {'title': inputExpensesTitle, 'value': inputExpensesAmount, 'expense': true};
     expensesList.push(temp);
     document.getElementById('table-body').innerHTML = '';
     createList();
+    changeColor();
+
+    //clear input boxes
+    document.getElementById('input-expenses-title').value = '';
+    document.getElementById('input-expenses-amount').value = '';
+    storage();
 }
 
 function createList() {
-    if (expensesList)
-    for (const expenses of expensesList) {
+    if (expensesList) {
         let index = 0;
-        if (!expenses.expense) {
-            document.getElementById('table-body').innerHTML += `<tr>
-            <td style="color: rgb(4, 155, 4)">${expenses.title}</td>
-            <td style="color: rgb(4, 155, 4)">+${expenses.value}</td>
-            <td>
-                <button class="edit-item"><i class="fa fa-edit"></i></button>
-            </td>
-            <td>
-                <button class="delete-item" onclick="removeItem(${index})"><i class="fa fa-trash"></i></button>
-            </td>
-            </tr>`; 
-        } else {
-            document.getElementById('table-body').innerHTML += `<tr>
-            <td style="color: rgb(150, 14, 14)">${expenses.title}</td>
-            <td style="color: rgb(150, 14, 14)">-${expenses.value}</td>
-            <td>
-                <button class="edit-item"><i class="fa fa-edit"></i></button>
-            </td>
-            <td>
-                <button class="delete-item" onclick="removeItem(${index})"><i class="fa fa-trash"></i></button>
-            </td>
-            </tr>`;
+        for (const expenses of expensesList) {
+            if (!expenses.expense) {
+                document.getElementById('table-body').innerHTML += `<tr>
+                <td><input value="${expenses.title}" id="item-title-${index}" style="color: rgb(4, 155, 4); border: none; background: none;" disabled></input></td>
+                <td style="color: rgb(4, 155, 4)">+${expenses.value}</td>
+                <td>
+                    <button class="edit-item" onclick="editItem(${index})"><i class="fa fa-edit"></i></button>
+                </td>
+                <td>
+                    <button class="delete-item" onclick="removeItem(${index})"><i class="fa fa-trash"></i></button>
+                </td>
+                </tr>`; 
+            } else {
+                document.getElementById('table-body').innerHTML += `<tr>
+                <td style="color: rgb(150, 14, 14)" contenteditable="true">${expenses.title}</td>
+                <td style="color: rgb(150, 14, 14)" contenteditable="true">-${expenses.value}</td>
+                <td>
+                    <button class="edit-item"><i class="fa fa-edit"></i></button>
+                </td>
+                <td>
+                    <button class="delete-item" onclick="removeItem(${index})"><i class="fa fa-trash"></i></button>
+                </td>
+                </tr>`;
+            }
+            index++;
         }
-
-        index++;
-              
     }
 }
-
+function editItem(index) {
+}
 function removeItem(index) {
+    if (expensesList[index].expense) {
+        expensesAmount -= Number(expensesList[index].value);
+        currentBalance += Number(expensesList[index].value);
+        updateBalance();
+    } else {
+        initialBalance -= Number(expensesList[index].value);
+        currentBalance -= Number(expensesList[index].value);
+        updateBalance();
+    }
     expensesList.splice(index, 1);
     document.getElementById('table-body').innerHTML = '';
     createList();
+    changeColor();
+    storage();
+}
+function clearAll() {
+    expensesList.length = 0;
+    document.getElementById('table-body').innerHTML = '';
+    initialBalance = 0; 
+    expensesAmount = 0;
+    currentBalance = 0;
+
+    updateBalance();
+    changeColor();
+    storage();
+}
+
+// change colors and visuals of blances as they change
+function changeColor() {
+    if (currentBalance < 0) {
+        let prop = -1 * currentBalance;
+        document.getElementById("current-balance").innerHTML = `-$${prop}`
+
+        document.getElementById("current-balance").style.color = '#960e0e';
+    } else if (currentBalance === 0) {
+        document.getElementById("current-balance").style.color = 'black';
+    } else {
+        document.getElementById('current-balance').style.color = '#049b04';
+    }
+
+    if (initialBalance === 0) {
+        document.getElementById("initial-balance").style.color = 'black';
+    } else {
+        document.getElementById('initial-balance').style.color = '#049b04';
+    }
+    
+    if (expensesAmount === 0) {
+        document.getElementById('expenses').style.color = 'black';
+    } else {
+        document.getElementById('expenses').style.color = '#960e0e';
+    }
+}
+function updateBalance() {
+    document.getElementById('initial-balance').innerHTML = `$${initialBalance}`
+    document.getElementById('expenses').innerHTML = `$${expensesAmount}`
+    document.getElementById('current-balance').innerHTML = `$${currentBalance}`
+}
+
+function storage() {
+    localStorage.setItem('initialBalance', JSON.stringify(initialBalance));
+    localStorage.setItem('expensesAmount', JSON.stringify(expensesAmount));
+    localStorage.setItem('currentBalance', JSON.stringify(currentBalance));
+    localStorage.setItem("expensesList", JSON.stringify(expensesList));
 }
 
 
 
 createList();
+updateBalance();
+changeColor();
 
 document.getElementById('income-button').addEventListener('click', add);
 document.getElementById('expenses-button').addEventListener('click', subtract);
-
+document.getElementById('clear-button').addEventListener('click', clearAll);
 
 
